a2296ccc4fadf090cc9d29f3d504c1ef24040bc8
zhangjialegh <zjl1195185765@outlook.com>
2017-07-11 15:59:18 +0800

    Main continue

--- src/class/Side/Side.js
+++ src/class/Side/Side.js
@@ -12,12 +12,16 @@ const SubMenu=Menu.SubMenu;
 class Side extends React.Component{
   constructor(props) {
     super(props)
+    this.click=this.click.bind(this);
+  }
+  click(e){
+    console.log(e.target.href);
   }
     render(){
         let {collapsed,mode,onCollapse}=this.props;
+        let {click}=this;
         return (
              <Sider
-              breakpoint='lg'
               collapsible
               collapsed={collapsed}
               onCollapse={onCollapse}
@@ -27,10 +31,11 @@ class Side extends React.Component{
         <div className="logo" />
           <Menu theme="dark" mode={mode} defaultSelectedKeys={['1']}>
             <SubMenu key="sub1" title={<span><Icon type="user"/><span className="nav-text">User</span></span>}
+            onClick={click}
             >
-            <Menu.Item key='1'><Link to="/"><Icon type='user'/>首页</Link></Menu.Item>
-            <Menu.Item key='2'><Link to="/forecast"><Icon type='video-camera'/>预报天气</Link></Menu.Item>
-            <Menu.Item key='3'><Link to="/hoursly"><Icon type='upload'/>24小时天气</Link></Menu.Item>
+            <Menu.Item key='1' ><Link onClick={click} to="/"><Icon type='user'/>首页</Link></Menu.Item>
+            <Menu.Item key='2' ><Link onClick={click} to="/forecast"><Icon type='video-camera'/>预报天气</Link></Menu.Item>
+            <Menu.Item key='3' ><Link onClick={click} to="/hoursly"><Icon type='upload'/>24小时天气</Link></Menu.Item>
             </SubMenu>
 
             <SubMenu key="sub2" title={<span><Icon type="team"/><span className="nav-text">Team</span></span>}
